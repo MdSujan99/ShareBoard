5c6d6d95467297cf53a45ea2057c7f4ca2ac7939
md sujan <khans8962@gmail.com>
2021-09-10 00:46:07 +0530

    first commit

---
+++ public/sketch.js
@@ -0,0 +1,32 @@
+var socket;
+
+function setup(){
+   createCanvas(windowWidth-50, windowHeight-50);
+   background(255);
+   socket = io.connect("http://localhost:1234");
+   socket.on('mouseReply', newDraw);
+}
+
+function newDraw(data){
+   noStroke();
+   fill(0,0,255);
+   ellipse(data.x, data.y, 20, 20);
+}
+
+function mouseDragged(){
+   // console.log(mouseX,mouseY);
+   noStroke();
+   fill(255,0,0);
+   ellipse(mouseX, mouseY, 20, 20);
+
+   var data = {
+      x: mouseX,
+      y: mouseY
+   };
+   // console.log("sending mouse data");
+   socket.emit('mouse', data);
+}
+
+function draw(){
+
+}
