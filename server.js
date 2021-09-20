086a402fb099cd2d1912e70cba5be1fa2fe56377
md sujan <khans8962@gmail.com>
2021-09-20 21:52:28 +0530

    second commit change port to 80 heroku

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-var express = require('express');
-var socket = require('socket.io');
+var express = require("express");
+var socket = require("socket.io");
 
-const myPort = 1234;
+const myPort = 80;
+// const myPort = 1234;
 
 //run the server
 var app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 var server = app.listen(myPort);
-if(server)
-	console.log("Server is running on port"+myPort);
+if (server) console.log("Server is running on port" + myPort);
 
 //socket  setup
 var io = socket(server);
 //when a new connection is made
-io.sockets.on('connection',newConnection);
-function newConnection(socket){
-		console.log("new connection\nsocket id:"+socket.id+"\n");
+io.sockets.on("connection", newConnection);
+function newConnection(socket) {
+  console.log("new connection\nsocket id:" + socket.id + "\n");
 
-		socket.on('mouse', mouseMessage);
+  socket.on("mouse", mouseMessage);
 
-		function mouseMessage(data){
-			console.log(data);
-			socket.broadcast.emit('mouseReply',data);
-		}
+  function mouseMessage(data) {
+    console.log(data);
+    socket.broadcast.emit("mouseReply", data);
+  }
 }
