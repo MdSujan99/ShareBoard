520b2ccd6a35941d2e896fb57ad22f5032acd56b
md sujan <khans8962@gmail.com>
2021-09-22 21:02:35 +0530

    commit 3

--- server.js
+++ server.js
@@ -1,26 +1,36 @@
-var express = require('express');
-var socket = require('socket.io');
+var express = require("express");
+var socket = require("socket.io");
 
-const myPort = 1234;
+// const myPort = 1234;
 
 //run the server
 var app = express();
-app.use(express.static('public'));
-var server = app.listen(myPort);
-if(server)
-	console.log("Server is running on port"+myPort);
+app.use(express.static("public"));
+var server = app.listen(3000, listen);
+
+function listen() {
+  //   var host = server.address().address;
+  //   var port = server.address().port;
+  var host = "localhost";
+  var port = 3000;
+  console.log("Server is listening on http//" + host + ":" + port);
+}
+
+app.use(express.static("public"));
 
 //socket  setup
 var io = socket(server);
+
 //when a new connection is made
-io.sockets.on('connection',newConnection);
-function newConnection(socket){
-		console.log("new connection\nsocket id:"+socket.id+"\n");
+io.sockets.on("connection", newConnection);
+function newConnection(socket) {
+  console.log("new connection\nsocket id:" + socket.id + "\n");
 
-		socket.on('mouse', mouseMessage);
-
-		function mouseMessage(data){
-			console.log(data);
-			socket.broadcast.emit('mouseReply',data);
-		}
+  socket.on("mouse", function (data) {
+    console.log(data);
+    socket.broadcast.emit("mouseReply", data);
+  });
+  socket.on("disconnect", function () {
+    console.log("Cient has disconnected");
+  });
 }
