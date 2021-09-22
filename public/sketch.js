bc6fad41d90451bc6bf0be5c03fc1af399f042c7
md sujan <khans8962@gmail.com>
2021-09-22 20:24:18 +0530

    second commit

--- public/sketch.js
+++ public/sketch.js
@@ -1,30 +1,32 @@
 var socket;
 
-function setup() {
-  createCanvas(windowWidth - 50, windowHeight - 50);
-  background(255);
-  socket = io.connect("http://0.0.0.0:" + process.env.PORT);
-  socket.on("mouseReply", newDraw);
+function setup(){
+   createCanvas(windowWidth-50, windowHeight-50);
+   background(255);
+   socket = io.connect("http://localhost:1234");
+   socket.on('mouseReply', newDraw);
 }
 
-function newDraw(data) {
-  noStroke();
-  fill(0, 0, 255);
-  ellipse(data.x, data.y, 20, 20);
+function newDraw(data){
+   noStroke();
+   fill(0,0,255);
+   ellipse(data.x, data.y, 20, 20);
 }
 
-function mouseDragged() {
-  // console.log(mouseX,mouseY);
-  noStroke();
-  fill(255, 0, 0);
-  ellipse(mouseX, mouseY, 20, 20);
+function mouseDragged(){
+   // console.log(mouseX,mouseY);
+   noStroke();
+   fill(255,0,0);
+   ellipse(mouseX, mouseY, 20, 20);
 
-  var data = {
-    x: mouseX,
-    y: mouseY,
-  };
-  // console.log("sending mouse data");
-  socket.emit("mouse", data);
+   var data = {
+      x: mouseX,
+      y: mouseY
+   };
+   // console.log("sending mouse data");
+   socket.emit('mouse', data);
 }
 
-function draw() {}
+function draw(){
+
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var socket = require('socket.io');
+
+const myPort = 1234;
+
+//run the server
+var app = express();
+app.use(express.static('public'));
+var server = app.listen(myPort);
+if(server)
+	console.log("Server is running on port"+myPort);
+
+//socket  setup
+var io = socket(server);
+//when a new connection is made
+io.sockets.on('connection',newConnection);
+function newConnection(socket){
+		console.log("new connection\nsocket id:"+socket.id+"\n");
+
+		socket.on('mouse', mouseMessage);
+
+		function mouseMessage(data){
+			console.log(data);
+			socket.broadcast.emit('mouseReply',data);
+		}
+}
