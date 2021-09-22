b61e2118ea110b4218f5a78d1d67255caf40df04
md sujan <khans8962@gmail.com>
2021-09-22 21:34:20 +0530

    commit to heroku

--- public/sketch.js
+++ public/sketch.js
@@ -3,7 +3,7 @@ var myPort = 3000;
 function setup() {
   createCanvas(windowWidth - 50, windowHeight - 50);
   background(255);
-  socket = io.connect("http://localhost:" + myPort);
+  socket = io.connect("http://0.0.0.0:" + myPort);
   socket.on("mouseReply", newDraw);
 }
 
